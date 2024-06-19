e66b092e6042761dfd02b14ce45fea91ebd45bc9
Supriya@1612 <npriya1227@gmail.com>
2024-06-19 10:25:27 -0500

    initial code

---
+++ src/main/java/task/palindrome.java
@@ -0,0 +1,10 @@
+package task;
+
+public class palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/task/printevennumbers.java
@@ -0,0 +1,14 @@
+package task;
+
+public class printevennumbers {
+
+public static void main(String args[]) {
+for (int i=1;i<=10;i++)
+{
+	if(i%2 == 0)
+	{
+		System.out.println(i);
+	}
+	}
+}
+}
